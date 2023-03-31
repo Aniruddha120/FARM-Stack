9675417c2783e494a0b4e610aa5924d528d71f1d
ASAR <aniruddhas588@gmail.com>
2023-03-31 12:33:26 +0600

    1st commit on fastapi

---
+++ FastApi/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index(): 
+    return('Hello World')
+@app.get('/welcome')
+def name():
+    return{'FastApi Tutorial'}
+@app.get('/thanks')
+def thanks(author : str):
+    return{'Author': f'{author}'}
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+
+
+
+
